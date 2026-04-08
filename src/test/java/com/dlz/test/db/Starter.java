901d38f7a7d0b65afbbf470d0330ba61bfdc73e6
丁奎 <59461202@qq.com>
2026-04-08 10:46:34 +0800

    增加jdk兼容性，@PostConstruct修改成继承InitializingBean

--- src/test/java/com/dlz/test/db/Starter.java
+++ src/test/java/com/dlz/test/db/Starter.java
@@ -3,21 +3,19 @@ package com.dlz.test.db;
 //import com.dlz.db.DbInfo;
 
 import com.dlz.db.holder.DBHolder;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-import javax.annotation.PostConstruct;
-
 
 @ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = {"com.dlz.spring", "com.dlz.test.db.config"})
-public class Starter {
+public class Starter implements InitializingBean {
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
     }
-    @PostConstruct
-    void init(){
+    public void afterPropertiesSet() throws Exception{
         DBHolder.getDao();
 //        String sql = DbInfo.getSql("key.setting.getSettings");
 //        System.out.println(sql);
